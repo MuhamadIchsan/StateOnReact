0c26201b9edfce4b6246fda14bf5862b2565bf59
MuhamadIchsan <mhmdiksan2121@gmail.com>
2021-04-28 23:04:00 +0700

    add logo on landing page

--- src/components/RouterComponent.jsx
+++ src/components/RouterComponent.jsx
@@ -4,12 +4,16 @@ import FunctionComponent from './FunctionComponent'
 import HomeComponent from './HomeComponent'
 import ListComponent from './ListComponent'
 import UsersComponent from './UsersComponent'
+import Logo from '../logo.svg';
 export default function RouterComponent() {
     return (
         <div>
             <Switch>
                 <Route exact path="/">
-                    <h1 className="text-center text-primary">Latihan aja sih</h1>
+                    <div className="text-center">
+                        <img src={Logo} alt="gambar" width="200" />
+                        <h1 className="text-primary">Latihan aja sih</h1>
+                    </div>
                 </Route>
                 <Route path="/stateManage">
                     <HomeComponent />
